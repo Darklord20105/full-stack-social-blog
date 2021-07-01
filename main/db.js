5e254da655d2ce09d17c85b5959c2b3f057a7ae6
Darklord20105 <darklord20105@gmail.com>
2021-07-01 19:24:21 +0300

    first deploy 8

--- main/db.js
+++ main/db.js
@@ -5,7 +5,10 @@ const isProduction = process.env.NODE_ENV === 'production'
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-    connectionString: `${process.env.DATABASE_URL}`
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+        }
 })
 // const pool = new Pool({
 //     connectionString: isProduction ? process.env.DATABASE_URL : connectionString
